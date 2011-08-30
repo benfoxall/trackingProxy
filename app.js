6d9e6b90a927ba66b99c604b74dd4468e267e41c
Ben Foxall <benfoxall@gmail.com>
2011-08-31 00:59:25 +0100

    made an example/dummy response from the app

--- app.js
+++ app.js
@@ -38,5 +38,10 @@ app.get('/', function(req, res){
 });
 */
 
+app.get('/_', function(req,res){
+	res.header('Content-type', 'application/json');
+	res.send('{"urls":["www.example.com/page1.htm","www.example.com/page2.htm","www.example.com/pagex.php"]}');
+})
+
 app.listen(3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
